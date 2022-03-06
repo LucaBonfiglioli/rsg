6f9a13170f330a0bfc6ee540812496c6c94dd9b8
Luca Bonfiglioli <Luca.Bonfiglioli@gmail.com>
2022-03-06 22:06:45 +0100

    Bump version: 0.1.0 → 0.1.1

--- rsg/__init__.py
+++ rsg/__init__.py
@@ -2,5 +2,5 @@ from rsg.core import Rsg, generator
 
 __author__ = "Luca Bonfiglioli"
 __email__ = "Luca.Bonfiglioli@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = [Rsg, generator]
